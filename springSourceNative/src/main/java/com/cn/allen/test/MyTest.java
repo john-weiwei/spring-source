1b867d49e93b96957df116309141c9cc1158231b
unitimes <Allen@X-MacBook-Pro.local>
2020-12-08 19:02:46 +0800

    update:新增源码模块

---
+++ springSourceNative/src/main/java/com/cn/allen/test/MyTest.java
@@ -0,0 +1,47 @@
+package com.cn.allen.test;
+
+
+import com.cn.allen.bean.Student;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * @Author:ZhangWeiWei
+ * @Date:2020/11/27
+ * @Description:
+ */
+public class MyTest {
+
+    /**
+     * spring 扫描注解的应用上下文
+     */
+    @Test
+    public void test1() {
+        //在单元测试中使用
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext("com.cn.allen.bean");
+        Student student = (Student) applicationContext.getBean("student");
+        System.out.println(student.getUsername());
+    }
+
+    /**
+     * 基于xml文件的上下文
+     */
+    @Test
+    public void test2() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Student student = (Student) applicationContext.getBean("student");
+        System.out.println(student.getUsername());
+    }
+
+    public void test3() {
+        //文件上下文，基本不用
+        new FileSystemXmlApplicationContext("");
+    }
+
+    public void test4() {
+        //内嵌的web上下文
+    }
+}
